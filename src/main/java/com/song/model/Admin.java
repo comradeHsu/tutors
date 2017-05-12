e8d1c6ad69d0745a25ee8d5a9b6e64e36ff01c85
knockinHeavenDoor <61239289@qq.com>
2017-05-12 11:23:34 +0800

    xuhui
    Signed-off-by: knockinHeavenDoor <61239289@qq.com>

--- src/main/java/com/song/model/Admin.java
+++ src/main/java/com/song/model/Admin.java
@@ -5,7 +5,40 @@ package com.song.model;
  */
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Admin {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String userName;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

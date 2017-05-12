50584df1af52154952179befcb75a6e2cf04ad53
knockinHeavenDoor <61239289@qq.com>
2017-05-12 10:30:48 +0800

    demo
    Signed-off-by: knockinHeavenDoor <61239289@qq.com>

---
+++ src/main/java/com/song/TutorApplication.java
@@ -0,0 +1,12 @@
+package com.song;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorApplication.class, args);
+	}
+}
